e7418bd049230d17e01f960a8ab18b248c9c46c0
hunainahmed777 <161420260+hunainahmed777@users.noreply.github.com>
2024-07-30 22:01:26 +0500

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,13 @@
+adj = input("enter adjective: ")
+verb1 = input("enter verb1: ")
+verb2 = input("enter verb2: ")
+famous = input("enter famous person name: ")
+
+
+
+
+
+madlib = (f"computer programing so {adj}! It makes me so excited all the time because  "
+          f"i love to {verb1}. stay hydrated and {verb2} like you are {famous}")
+
+print(madlib)
